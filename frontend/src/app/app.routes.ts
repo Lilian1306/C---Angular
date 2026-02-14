afbacfa11b3b8ec83130a75b8104afb9208db98d
DaniM0613 <wwh.rj.92@gmail.com>
2026-02-14 15:41:48 -0600

    Creating the components and the task.services.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TaskForm } from './components/task-form/task-form';
+import { TaskList } from './components/task-list/task-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [   
+    {path: "", component: TaskForm},
+    {path: "add-task", component: TaskList}                
+]
